8a6d406c825e90d77f2a88fbbf46567970dcaaa2
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-07 15:37:09 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         orange: '#ff8c2b',
-        dark: '#0b0b0f',
-        panel: '#141419'
-      },
-    },
+        panel: '#141419',
+        bg: '#0b0b0f'
+      }
+    }
   },
+  plugins: []
 }
