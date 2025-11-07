8ff7a871ac6c86edbadc2ff0dc337e87cd760023
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-07 15:03:52 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,37 @@
 // pages/_app.js
 import '../styles/globals.css'
-import WagmiProvider from '../components/WagmiProvider'
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { mainnet } from 'viem/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { publicClient } from 'viem'
+import Layout from '../components/Sidebar'
 
-function MyApp({ Component, pageProps }) {
+const ALCHEMY = process.env.NEXT_PUBLIC_ALCHEMY_KEY
+const WC_PROJECT = process.env.NEXT_PUBLIC_WC_PROJECT_ID
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: ALCHEMY }), publicProvider()]
+)
+
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new (await import('wagmi/connectors/injected')).InjectedConnector({ chains }),
+    new WalletConnectConnector({ chains, options: { projectId: WC_PROJECT } })
+  ],
+  publicClient: provider,
+  webSocketPublicClient: webSocketProvider,
+})
+
+export default function App({ Component, pageProps }) {
   return (
-    <WagmiProvider>
-      <Component {...pageProps} />
-    </WagmiProvider>
+    <WagmiConfig config={config}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </WagmiConfig>
   )
 }
-
-export default MyApp
