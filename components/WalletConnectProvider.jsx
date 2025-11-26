236d8c3caf86f1eedfb04ba8e7b9bc339f2f099a
Emmanuel Adams <46665800+AceBoss1@users.noreply.github.com>
2025-11-27 00:21:47 +0100

    Delete components/WalletConnectProvider.jsx

--- components/WalletConnectProvider.jsx
+++
@@ -1,7 +0,0 @@
-// components/WagmiProvider.jsx
-import { WagmiConfig } from 'wagmi'
-import { wagmiClient } from '../lib/wagmiClient'
-
-export default function WagmiProvider({ children }) {
-  return <WagmiConfig client={wagmiClient}>{children}</WagmiConfig>
-}
